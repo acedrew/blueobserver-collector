f0bd2630ed8db6a8699a60633c36238152ea25ac
Andrew Rodgers <acedrew@gmail.com>
2017-04-16 21:08:59 +0000

    added google geo locator service

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import requests
 import base64
 import os
 import multiprocessing
+from wifi import Cell
 
 salt = os.environ['BLUE_COLLECTOR_HASH_SALT']
 location = os.environ['BLUE_COLLECTOR_LOCATION_NAME']
-if 'BLUE_COLLECTOR_GEO_POINT' in os.environ:
-    geo_point = [float(a) for a in os.environ[
-        'BLUE_COLLECTOR_GEO_POINT'].split(',')]
+geo_point_str = os.environ.get('BLUE_COLLECTOR_GEO_POINT', "0.0,0.0")
+geo_point = [float(a) for a in geo_point_str.split(',')]
+
+geo_locator = os.environ.get('BLUE_COLLECTOR_GEO_SOURCE', None)
+if geo_locator:
+    geo_api_key = os.environ['BLUE_COLLECTOR_GEO_API_KEY']
 
 
 class DeviceScanner(multiprocessing.Process):
@@ -63,12 +67,21 @@ class DevicePublisher(multiprocessing.Process):
         self.observations = observation_queue
         self.seen_devices = {}
         self.cleanup_counter = 0
+        self.geo_point = geo_point
 
     def run(self):
         while True:
             observation = self.observations.get()
             self.process_observation(observation)
             print(observation)
+            try:
+                location = self.locations.get(block=False)
+                self.process_location(location)
+            except:
+                pass
+
+    def process_location(self, location):
+        self.geo_point = location
 
     def process_observation(self, observation):
         (mac, obs_time) = observation
@@ -99,7 +112,7 @@ class DevicePublisher(multiprocessing.Process):
         observation['hash'] = base64.urlsafe_b64encode(
             argon2.argon2_hash(mac, salt)).decode()
         observation['location'] = location
-        observation['geo_point'] = geo_point
+        observation['geo_point'] = self.geo_point
         observation['timestamp'] = obs_time
         try:
             r = requests.post("http://localhost:5000/observations",
@@ -107,15 +120,56 @@ class DevicePublisher(multiprocessing.Process):
         except Exception as e:
             print('publish failed for observation: ' + observation['hash'] +
                   'with exception: ' + str(e) + " http code: " + r.code)
+            self.observations.put((mac, obs_time))
             pass
+
+
+class LocationService(multiprocessing.Process):
+
+    def __init__(self, location_queue, api_key, interval=60):
+        multiprocessing.Process.__init__(self)
+        self.locations = location_queue
+        self.last_scan = 0
+        self.interval = interval
+        self.geo_api_url = ('https://www.googleapis.com/geolocation/v1/'
+                            'geolocate?key=') + api_key
+
+    def run(self):
+        while True:
+            now = time.time()
+            if now > self.last_scan + self.interval:
+                self.get_location()
+                self.last_scan = now
+            time.sleep(5)
+
+    def get_location(self):
+        ap_scan = Cell.all('wlan0')
+        aps = []
+        for bssid in ap_scan:
+            ap = {}
+            ap['macAddress'] = bssid.address
+            ap['signalStrength'] = bssid.signal
+            ap['channel'] = bssid.channel
+            aps.append(ap)
+        r = requests.post(self.geo_api_url,  json={"wifiAccessPoints": aps,
+                                                   "considerIp": "false"
+                                                   })
+        if r.code == 200:
+            response = r.json()
+            location = (response.location.lat, response.location.lng)
+            self.locations.put(location)
 
 
 def main():
     print(geo_point)
     observations = multiprocessing.Queue()
+    locations = multiprocessing.Queue()
     ble_scanner = BLEScanner(observations)
     bt_scanner = BTScanner(observations)
-    publisher = DevicePublisher(observations)
+    publisher = DevicePublisher(observations, locations)
+    if geo_locator:
+        locator = LocationService(locations, geo_api_key)
+        locator.start()
     ble_scanner.start()
     bt_scanner.start()
     publisher.start()
